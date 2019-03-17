0783aa560f822b679269d67d2d7d137aeb97e787
Zhang Yuanqing <zhangyuanqing.generalzyq@gmail.com>
2019-03-17 23:33:59 +0100

    basic isolines class

--- Smoke/isolinesHelper.h
+++ Smoke/isolinesHelper.h
@@ -73,4 +73,107 @@ void linePointsInCell(float x1, float y1, float width, float height,float sepRho
 		*sepx4 = x1 + width;
 		*sepy4 = y1 + height / 2.0;
 	}
+	else if (rho3 >= sepRho && rho4 >= sepRho && rho1 < sepRho && rho2 < sepRho) // 0110
+	{
+		*sepx1 = x1 + width / 2.0;
+		*sepy1 = y1;
+
+		*sepx2 = x1 + width / 2.0;
+		*sepy2 = y1 + height;
+
+		*sepx3 = 0; *sepx4 = 0;
+		*sepy3 = 0; *sepy4 = 0;
+	}
+	else if (rho1 >= sepRho && rho3 >= sepRho && rho4 >= sepRho && rho2 < sepRho) // 0111
+	{
+		*sepx1 = x1;
+		*sepy1 = y1 + height / 2.0;
+
+		*sepx2 = x1 + width / 2.0;
+		*sepy2 = y1 + height;
+
+		*sepx3 = 0; *sepx4 = 0;
+		*sepy3 = 0; *sepy4 = 0;
+	}
+	else if (rho2 >= sepRho && rho1 < sepRho && rho3 < sepRho && rho4 < sepRho) // 1000
+	{
+		*sepx1 = x1;
+		*sepy1 = y1 + height / 2.0;
+
+		*sepx2 = x1 + width / 2.0;
+		*sepy2 = y1 + height;
+
+		*sepx3 = 0; *sepx4 = 0;
+		*sepy3 = 0; *sepy4 = 0;
+	}
+	else if (rho1 >= sepRho && rho2 >= sepRho && rho3 < sepRho && rho4 < sepRho) // 1001
+	{
+		*sepx1 = x1 + width / 2.0;
+		*sepy1 = y1;
+
+		*sepx2 = x1 + width / 2.0;
+		*sepy2 = y1 + height;
+
+		*sepx3 = 0; *sepx4 = 0;
+		*sepy3 = 0; *sepy4 = 0;
+	}
+	else if (rho2 >= sepRho && rho4 >= sepRho && rho1 < sepRho && rho3 < sepRho) // 1010
+	{
+		*sepx1 = x1;
+		*sepy1 = y1 + height / 2.0;
+
+		*sepx2 = x1 + width / 2.0;
+		*sepy2 = y1 + height;
+
+		*sepx3 = x1 + width / 2.0;
+		*sepy3 = y1;
+
+		*sepx4 = x1 + width;
+		*sepy4 = y1 + height / 2.0;
+	}
+	else if (rho1 >= sepRho && rho2 >= sepRho && rho4 >= sepRho && rho3 < sepRho) // 1011
+	{ 
+		*sepx1 = x1 + width / 2.0;
+		*sepy1 = y1 + height;
+
+		*sepx2 = x1 + width;
+		*sepy2 = y1 + height / 2.0;
+
+		*sepx3 = 0; *sepx4 = 0;
+		*sepy3 = 0; *sepy4 = 0;
+	}
+	else if (rho2 >= sepRho && rho3 >= sepRho && rho1 < sepRho && rho4 < sepRho) // 1100
+	{
+		*sepx1 = x1;
+		*sepy1 = y1 + height / 2.0;
+	
+		*sepx2 = x1 + width;
+		*sepy2 = y1 + height / 2.0;
+
+		*sepx3 = 0; *sepx4 = 0;
+		*sepy3 = 0; *sepy4 = 0;
+	}
+	else if (rho1 >= sepRho && rho2 >= sepRho && rho3 >= sepRho && rho4 < sepRho) // 1101
+	{
+		*sepx1 = x1 + width / 2.0;
+		*sepy1 = y1;
+
+		*sepx2 = x1 + width;
+		*sepy2 = y1 + height / 2.0;
+
+		*sepx3 = 0; *sepx4 = 0;
+		*sepy3 = 0; *sepy4 = 0;
+	}
+	else if (rho2 >= sepRho && rho3 >= sepRho && rho4 >= sepRho && rho1 < sepRho) // 1110
+	{
+
+		*sepx1 = x1;
+		*sepy1 = y1 + height / 2.0;
+
+		*sepx2 = x1 + width / 2.0;
+		*sepy2 = y1;
+
+		*sepx3 = 0; *sepx4 = 0;
+		*sepy3 = 0; *sepy4 = 0;
+	}
 }
